e3eb6757fc6253af59af25bee24b95044a4eaed8
haebeen <haebing0309@gmail.com>
2026-01-31 16:40:35 +0900

    Initial commit

---
+++ src/main/java/com/wemade/wemade/WemadeApplication.java
@@ -0,0 +1,13 @@
+package com.wemade.wemade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wemade/wemade/WemadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemade.wemade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
