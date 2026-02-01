df113933f8e5f2ec1b2540fde6d837bd80fc02ce
ParkHaeBeen <haebing0309@gmail.com>
2026-02-02 07:23:08 +0900

    refactor: redefine analysis domain structure

--- src/main/java/com/wemade/domain/AnalysisStatistics.java
+++ src/main/java/com/wemade/domain/AnalysisStatistics.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AnalysisStatistics {
+    private long success2xx;
+    private long redirect3xx;
+    private long clientError4xx;
+    private long serverError5xx;
     private final Map<String, Long> ip = new HashMap<>();
     private final Map<String, Long> path = new HashMap<>();
     private final Map<Integer, Long> status = new HashMap<>();
